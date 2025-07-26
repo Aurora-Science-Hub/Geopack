190a8b61eebfb7504df43f6ea510331c540f813c
alexander <demosfen.spb@gmail.com>
2025-07-26 09:20:00 +0300

    CR #2

--- UnitTests/Geopack2008/Geopack2008Tests.BSPCAR.cs
+++ UnitTests/Geopack2008/Geopack2008Tests.BSPCAR.cs
@@ -7,7 +7,7 @@ namespace AuroraScienceHub.Geopack.UnitTests.Geopack2008;
 public partial class Geopack2008Tests
 {
     [Fact(DisplayName = "BSPCAR_08: Compare with FORTRAN calculation")]
-    public async Task BSPCAR_08_ShouldBeCorrect()
+    public async Task BSPCAR_08_ReturnsExpectedValues()
     {
         // Arrange
         var rawData = await EmbeddedResourceReader.ReadTextAsync(BSpCarDatasetFileName);
@@ -38,10 +38,12 @@ public partial class Geopack2008Tests
     [Fact(DisplayName = "BSPCAR_08: Zero angles B-field coordinates conversion")]
     public void BSPCAR_08_ZeroAngles_ReturnsExpectedValues()
     {
+        // Act
         _geopack2008.BSPCAR_08(
             0.0, 0.0, 1.0, 1.0, 1.0,
             out double bx, out double by, out double bz);
 
+        // Assert
         bx.ShouldBe(1.0, MinimalTestsPrecision);
         by.ShouldBe(1.0, MinimalTestsPrecision);
         bz.ShouldBe(1.0, MinimalTestsPrecision);
@@ -50,10 +52,12 @@ public partial class Geopack2008Tests
     [Fact(DisplayName = "BSPCAR_08: Negative angles B-field coordinates conversion")]
     public void BSPCAR_08_NegativeAngles_ReturnsExpectedValues()
     {
+        // Act
         _geopack2008.BSPCAR_08(
             -Math.PI / 4, -Math.PI / 4, 1.0, 1.0, 1.0,
             out double bx, out double by, out double bz);
 
+        // Assert
         bx.ShouldBe(0.70710678118654757, MinimalTestsPrecision);
         by.ShouldBe(0.70710678118654746, MinimalTestsPrecision);
         bz.ShouldBe(1.4142135623730949, MinimalTestsPrecision);
@@ -62,10 +66,12 @@ public partial class Geopack2008Tests
     [Fact(DisplayName = "BSPCAR_08: Large angles B-field coordinates conversion")]
     public void BSPCAR_08_LargeAngles_ReturnsExpectedValues()
     {
+        // Act
         _geopack2008.BSPCAR_08(
             2 * Math.PI, 2 * Math.PI, 1.0, 1.0, 1.0,
             out double bx, out double by, out double bz);
 
+        // Assert
         bx.ShouldBe(1.0, MinimalTestsPrecision);
         by.ShouldBe(1.0, MinimalTestsPrecision);
         bz.ShouldBe(1.0, MinimalTestsPrecision);
@@ -74,10 +80,12 @@ public partial class Geopack2008Tests
     [Fact(DisplayName = "BSPCAR_08: Small angles B-field coordinates conversion")]
     public void BSPCAR_08_SmallAngles_ReturnsExpectedValues()
     {
+        // Act
         _geopack2008.BSPCAR_08(
             1e-10, 1e-10, 1.0, 1.0, 1.0,
             out double bx, out double by, out double bz);
 
+        // Assert
         bx.ShouldBe(1.0, MinimalTestsPrecision);
         by.ShouldBe(1.0000000001, MinimalTestsPrecision);
         bz.ShouldBe(0.99999999989999999, MinimalTestsPrecision);
