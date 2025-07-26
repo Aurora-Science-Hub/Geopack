304f093b4e01b0bbd23cdddad094ec7f49d57085
alexander <demosfen.spb@gmail.com>
2025-07-26 12:59:21 +0300

    Refactor Sun

--- src/Geopack/Geopack08/Geopack.Sun.cs
+++ src/Geopack/Geopack08/Geopack.Sun.cs
@@ -2,23 +2,20 @@ namespace AuroraScienceHub.Geopack.Geopack08;
 
 public sealed partial class Geopack08
 {
-    public void SUN_08(DateTime dateTime, out double gst, out double slong, out double srasn, out double sdec)
+    public Sun Sun(DateTime dateTime)
     {
-
-
-        if (dateTime.Year < 1901 || dateTime.Year > 2099)
+        if (dateTime.Year is < 1901 or > 2099)
         {
-            gst = slong = srasn = sdec = 0;
-            return;
+            return new Sun();
         }
 
         double fday = (dateTime.Hour * 3600 + dateTime.Minute * 60 + dateTime.Second) / 86400.0D;
         double dj = 365 * (dateTime.Year - 1900) + (dateTime.Year - 1901) / 4.0D + dateTime.DayOfYear - 0.5D + fday;
         double t = dj / 36525.0D;
         double vl = (279.696678D + 0.9856473354D * dj) % 360.0D;
-        gst = (279.690983D + 0.9856473354D * dj + 360.0D * fday + 180.0D) % 360.0D / Rad;
+        double gst = (279.690983D + 0.9856473354D * dj + 360.0D * fday + 180.0D) % 360.0D / Rad;
         double g = (358.475845D + 0.985600267D * dj) % 360.0D / Rad;
-        slong = (vl + (1.91946D - 0.004789D * t) * Math.Sin(g) + 0.020094D * Math.Sin(2.0D * g)) / Rad;
+        double slong = (vl + (1.91946D - 0.004789D * t) * Math.Sin(g) + 0.020094D * Math.Sin(2.0D * g)) / Rad;
 
         if (slong > 6.2831853D)
         {
@@ -36,7 +33,9 @@ public sealed partial class Geopack08
         double sind = sob * Math.Sin(slp);
         double cosd = Math.Sqrt(1.0D - sind * sind);
         double sc = sind / cosd;
-        sdec = Math.Atan2(sind, cosd);
-        srasn = Pi - Math.Atan2(Math.Cos(obliq) / sob * sc, -Math.Cos(slp) / cosd);
+        double sdec = Math.Atan2(sind, cosd);
+        double srasn = Pi - Math.Atan2(Math.Cos(obliq) / sob * sc, -Math.Cos(slp) / cosd);
+
+        return new Sun(gst, slong, srasn, sdec);
     }
 }
