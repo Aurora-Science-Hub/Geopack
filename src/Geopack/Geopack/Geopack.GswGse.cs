03001c152b4a9146e71751e47089dc635771e0ac
alexander <demosfen.spb@gmail.com>
2025-09-27 19:35:50 +0300

    +

--- src/Geopack/Geopack/Geopack.GswGse.cs
+++ src/Geopack/Geopack/Geopack.GswGse.cs
@@ -6,9 +6,9 @@ public sealed partial class Geopack
 {
     public CartesianLocation GswGse(double xgsw, double ygsw, double zgsw)
     {
-        double xgse = xgsw * Common1.E11 + ygsw * Common1.E21 + zgsw * Common1.E31;
-        double ygse = xgsw * Common1.E12 + ygsw * Common1.E22 + zgsw * Common1.E32;
-        double zgse = xgsw * Common1.E13 + ygsw * Common1.E23 + zgsw * Common1.E33;
+        double xgse = xgsw * Common1.E11 + ygsw * Common1.E12 + zgsw * Common1.E13;
+        double ygse = xgsw * Common1.E21 + ygsw * Common1.E22 + zgsw * Common1.E23;
+        double zgse = xgsw * Common1.E31 + ygsw * Common1.E32 + zgsw * Common1.E33;
         return new CartesianLocation(xgse, ygse, zgse);
     }
 }
