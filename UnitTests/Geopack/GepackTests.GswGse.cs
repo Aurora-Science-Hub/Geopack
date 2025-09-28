e836b2595a9e199d649c9614899cbdad4ebfe161
alexander <demosfen.spb@gmail.com>
2025-09-28 06:50:31 +0300

    +

--- UnitTests/Geopack/GepackTests.GswGse.cs
+++ UnitTests/Geopack/GepackTests.GswGse.cs
@@ -8,6 +8,7 @@ public partial class GeopackTests
     [InlineData(-6.5999999999999996, 0, 0.0, -6.593403902518470971, 0.281954772147567645, 0.086755313802676370)]
     [InlineData(1, 1, 1, 1.058072250173702544, 1.071148866358210050, 0.856226149857503227)]
     [InlineData(-1, -1, -1, -1.058072250173702544, -1.071148866358210050, -0.856226149857503227)]
+    [InlineData(4.5678, 4.5678, 4.5678, 4.833062424343439467, 4.892793791751032018, 3.911069807319103475)]
     public void GswGse_ReturnsCorrectValues(double xgsw, double ygsw, double zgsw, double xgse, double ygse, double zgse)
     {
         // Act
@@ -24,6 +25,7 @@ public partial class GeopackTests
     [InlineData( -6.5999999999999996, 0, 0, -6.593403902518470971, -0.269181070401027012, -0.120691878226939539)]
     [InlineData(1, 1, 1, 0.943135426752761630, 0.909404539846109805, 1.132907299790544497)]
     [InlineData(-1, -1, -1, -0.943135426752761630, -0.909404539846109805, -1.132907299790544497)]
+    [InlineData(4.5678, 4.5678, 4.5678, 4.308054002321264342, 4.153978057109060984, 5.174893963983248746)]
     public void GseGsw_ReturnsCorrectValues(double xgse, double ygse, double zgse, double xgsw, double ygsw, double zgsw)
     {
         // Act
