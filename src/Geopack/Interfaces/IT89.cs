2b6eb42eb55cbb064946d0a17bc9d625912aa164
alexander <demosfen.spb@gmail.com>
2025-10-01 08:59:50 +0300

    T89C basic double-precision model

--- src/Geopack/Interfaces/IT89.cs
+++ src/Geopack/Interfaces/IT89.cs
@@ -1,3 +1,5 @@
+using AuroraScienceHub.Geopack.Contracts;
+
 namespace AuroraScienceHub.Geopack.Interfaces;
 
 public interface IT89
@@ -9,25 +11,21 @@ public interface IT89
     /// The model is valid up to geocentric distances of 70 RE and is based on merged spacecraft data sets.
     /// </remarks>
     /// <param name="iopt">
-    /// Specifies the ground disturbance level:
-    /// IOPT= 1       2        3        4        5        6      7
-    /// CORRESPOND TO:
-    /// KP= 0,0+  1-,1,1+  2-,2,2+  3-,3,3+  4-,4,4+  5-,5,5+  >=6-
+    ///     Specifies the ground disturbance level:
+    ///     IOPT= 1       2        3        4        5        6      7
+    ///     CORRESPOND TO:
+    ///     KP= 0,0+  1-,1,1+  2-,2,2+  3-,3,3+  4-,4,4+  5-,5,5+  >=6-
     /// </param>
     /// <param name="parmod">
-    /// Dummy array, not used in this subroutine, provided for compatibility with the new version of the GEOPACK software.
+    ///     Dummy array, not used in this subroutine, provided for compatibility with the new version of the GEOPACK software.
     /// </param>
     /// <param name="ps">Geodipole tilt angle in radians</param>
     /// <param name="x">GSM x-coordinate in Earth radii</param>
     /// <param name="y">GSM y-coordinate in Earth radii</param>
     /// <param name="z">GSM z-coordinate in Earth radii</param>
-    /// <param name="bx">GSM x-component of the model magnetic field in nanoteslas</param>
-    /// <param name="by">GSM y-component of the model magnetic field in nanoteslas</param>
-    /// <param name="bz">GSM z-component of the model magnetic field in nanoteslas</param>
-    void T89D_SP(
+    CartesianFieldVector T89D_SP(
         int iopt,
         float[] parmod,
         float ps,
-        float x, float y, float z,
-        out float bx, out float by, out float bz);
+        float x, float y, float z);
 }
--- src/Geopack/T89/T89.cs
+++ src/Geopack/T89/T89.cs
@@ -1,14 +1,10 @@
+using AuroraScienceHub.Geopack.Contracts;
 using AuroraScienceHub.Geopack.Interfaces;
 
 namespace AuroraScienceHub.Geopack.T89;
 
 public sealed class T89 : IT89
 {
-    public void T89D_SP(
-        int iopt,
-        float[] parmod,
-        float ps,
-        float x, float y, float z,
-        out float bx, out float by, out float bz)
-        => throw new NotImplementedException();
+    public CartesianFieldVector T89D_SP(int iopt, float[] parmod, float ps, float x, float y, float z)
+        => new(0.0D, 0.0D, 0.0D, CoordinateSystem.GSW);
 }
